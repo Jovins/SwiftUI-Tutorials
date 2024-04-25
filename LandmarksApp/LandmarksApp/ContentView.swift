b015ea32ea3abc9112baa70377c503d6f2c59fe5
Jovins <jovinscoder@gmail.com>
2024-04-25 14:18:18 +0800

    create landmark app

---
+++ LandmarksApp/LandmarksApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LandmarksApp
+//
+//  Created by Jovins on 2024/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
